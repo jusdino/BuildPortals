4bbdb5238045dbda8909b7b7607099725659197c
Justin Frahm <justinmfrahm@gmail.com>
2016-02-11 21:39:01 -0700

    starting repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# BuildPortals
+Bukkit/Spigot plugin to allow players to build their own inter-world portals
+
+Feb 11, 2015 - This project is still very much in progress. I'm learning as I go here. If someone happens across this project, feel free to critique. I welcome tips/criticism/suggestions!
---
+++ src/main/java/frahm/justin/mcplugins/BuildPortals/Main.java
@@ -0,0 +1,40 @@
+package frahm.justin.mcplugins.BuildPortals;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new BPListener(this), this);
+		//FileConfiguration config = this.getConfig();
+		//config.addDefault("youAreAwesome", true);
+		//config.options().copyDefaults(true);
+		this.saveConfig();
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("vec")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Only players can ask for their vector.");
+				return false;
+			}
+			Player player = (Player) sender;
+			player.sendMessage("Your vector is: " + player.getLocation().toVector().toString());
+			player.sendMessage("   as a string: " + player.getLocation().toString());
+			return true;
+		}
+		return false;
+	}
+}
