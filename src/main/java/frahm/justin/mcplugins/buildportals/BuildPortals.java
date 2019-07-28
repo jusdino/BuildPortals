5cea07627ae4286fbffe5f4b895674f5f6ac1155
Justin Frahm <justinmfrahm@gmail.com>
2019-07-28 15:57:09 -0600

    Add debug toggle command

--- src/main/java/frahm/justin/mcplugins/buildportals/BuildPortals.java
+++ src/main/java/frahm/justin/mcplugins/buildportals/BuildPortals.java
@@ -214,6 +214,11 @@ public class BuildPortals extends JavaPlugin {
 						sender.sendMessage("You do not have permission to use this command.");
 						return true;
 					}
+				case "debug":
+					if (sender.hasPermission("buildportals.*")) {
+						config.set("debug", config.getBoolean("debug"));
+						sender.sendMessage("Debug: " + config.getBoolean("debug"));
+					}
 				default:
 					sender.sendMessage("BuildPortals command usage:");
 					sender.sendMessage("  /BP Version ");
